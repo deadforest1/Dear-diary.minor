de27e8e10e4c32d164e929c49074f10286202d2c
l_isupova <is.olya9@gmail.com>
2024-11-05 12:32:58 +0300

    Add files via upload

---
+++ scripts/task.js
@@ -0,0 +1,21 @@
+// Дано вещественное число A. Вычислить х = a**2+4+5  при а < 62; в противном случае x = 1/(a**2) + 4a + 5).
+function verify(){
+    console.log("a")
+    let a = parseFloat(elementA.value);
+    console.log(a)
+    
+    if (a < 62) {
+        x = a**2+4+5
+        document.getElementById('x').innerText = x;}
+    else {
+        x = 1/((a**2) + 4*a + 5)
+        document.getElementById('x').innerText = x;}
+       
+    }
+    
+elementA = document.getElementById('a');
+elementVerify = document.getElementById('verify');
+elementVerify.addEventListener('click', verify);
+
+
+ 
